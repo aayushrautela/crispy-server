61ef0fff812be44d46f22bdfe3f4074a9fffd73c
Aayush Rautela <rautela4488@gmail.com>
2026-04-01 19:22:12 +0200

    Fix alias reference: wrap computed columns so WHERE can use them

--- migrations/0022_watch_media_card_cache.sql
+++ migrations/0022_watch_media_card_cache.sql
@@ -30,42 +30,31 @@ INSERT INTO watch_media_card_cache (
     rating,
     updated_at
 )
-SELECT media_key,
-       media_type,
-       CASE
-         WHEN media_type IN ('movie', 'show', 'anime') THEN split_part(media_key, ':', 2)
-         ELSE coalesce(playback_parent_provider, split_part(media_key, ':', 2))
-       END AS title_provider,
-       CASE
-         WHEN media_type IN ('movie', 'show', 'anime') THEN split_part(media_key, ':', 3)
-         ELSE coalesce(playback_parent_provider_id, split_part(media_key, ':', 3))
-       END AS title_provider_id,
-       CASE
-         WHEN details_title_media_type IN ('movie', 'show', 'anime') THEN details_title_media_type
-         WHEN media_type = 'anime' THEN 'anime'
-         WHEN media_type = 'movie' THEN 'movie'
-         ELSE 'show'
-       END AS title_media_type,
-       title,
-       subtitle,
-       poster_url,
-       backdrop_url,
-       details_release_year,
-       details_rating,
-       now()
+SELECT *
 FROM (
     SELECT media_key,
            media_type,
-           details_title_media_type,
-           playback_parent_provider,
-           playback_parent_provider_id,
+           CASE
+             WHEN media_type IN ('movie', 'show', 'anime') THEN split_part(media_key, ':', 2)
+             ELSE coalesce(playback_parent_provider, split_part(media_key, ':', 2))
+           END AS title_provider,
+           CASE
+             WHEN media_type IN ('movie', 'show', 'anime') THEN split_part(media_key, ':', 3)
+             ELSE coalesce(playback_parent_provider_id, split_part(media_key, ':', 3))
+           END AS title_provider_id,
+           CASE
+             WHEN details_title_media_type IN ('movie', 'show', 'anime') THEN details_title_media_type
+             WHEN media_type = 'anime' THEN 'anime'
+             WHEN media_type = 'movie' THEN 'movie'
+             ELSE 'show'
+           END AS title_media_type,
            title,
            subtitle,
            poster_url,
            backdrop_url,
            details_release_year,
            details_rating,
-           row_number() OVER (PARTITION BY media_key ORDER BY last_activity_at DESC NULLS LAST, watched_at DESC NULLS LAST, added_at DESC NULLS LAST, rated_at DESC NULLS LAST) AS row_rank
+           now()
     FROM (
         SELECT media_key,
                media_type,
@@ -78,68 +67,82 @@ FROM (
                backdrop_url,
                details_release_year,
                details_rating,
-               last_activity_at,
-               NULL::timestamptz AS watched_at,
-               NULL::timestamptz AS added_at,
-               NULL::timestamptz AS rated_at
-        FROM continue_watching_projection
-        UNION ALL
-        SELECT media_key,
-               media_type,
-               details_title_media_type,
-               playback_parent_provider,
-               playback_parent_provider_id,
-               title,
-               subtitle,
-               poster_url,
-               backdrop_url,
-               details_release_year,
-               details_rating,
-               NULL::timestamptz,
-               watched_at,
-               NULL::timestamptz,
-               NULL::timestamptz
-        FROM watch_history
-        UNION ALL
-        SELECT media_key,
-               media_type,
-               details_title_media_type,
-               playback_parent_provider,
-               playback_parent_provider_id,
-               title,
-               subtitle,
-               poster_url,
-               backdrop_url,
-               details_release_year,
-               details_rating,
-               NULL::timestamptz,
-               NULL::timestamptz,
-               added_at,
-               NULL::timestamptz
-        FROM watchlist_items
-        UNION ALL
-        SELECT media_key,
-               media_type,
-               details_title_media_type,
-               playback_parent_provider,
-               playback_parent_provider_id,
-               title,
-               subtitle,
-               poster_url,
-               backdrop_url,
-               details_release_year,
-               details_rating,
-               NULL::timestamptz,
-               NULL::timestamptz,
-               NULL::timestamptz,
-               rated_at
-        FROM ratings
-    ) seeded_rows
-) deduped_rows
-WHERE row_rank = 1
-  AND title IS NOT NULL
-  AND poster_url IS NOT NULL
-  AND title_provider <> ''
+               row_number() OVER (PARTITION BY media_key ORDER BY last_activity_at DESC NULLS LAST, watched_at DESC NULLS LAST, added_at DESC NULLS LAST, rated_at DESC NULLS LAST) AS row_rank
+        FROM (
+            SELECT media_key,
+                   media_type,
+                   details_title_media_type,
+                   playback_parent_provider,
+                   playback_parent_provider_id,
+                   title,
+                   subtitle,
+                   poster_url,
+                   backdrop_url,
+                   details_release_year,
+                   details_rating,
+                   last_activity_at,
+                   NULL::timestamptz AS watched_at,
+                   NULL::timestamptz AS added_at,
+                   NULL::timestamptz AS rated_at
+            FROM continue_watching_projection
+            UNION ALL
+            SELECT media_key,
+                   media_type,
+                   details_title_media_type,
+                   playback_parent_provider,
+                   playback_parent_provider_id,
+                   title,
+                   subtitle,
+                   poster_url,
+                   backdrop_url,
+                   details_release_year,
+                   details_rating,
+                   NULL::timestamptz,
+                   watched_at,
+                   NULL::timestamptz,
+                   NULL::timestamptz
+            FROM watch_history
+            UNION ALL
+            SELECT media_key,
+                   media_type,
+                   details_title_media_type,
+                   playback_parent_provider,
+                   playback_parent_provider_id,
+                   title,
+                   subtitle,
+                   poster_url,
+                   backdrop_url,
+                   details_release_year,
+                   details_rating,
+                   NULL::timestamptz,
+                   NULL::timestamptz,
+                   added_at,
+                   NULL::timestamptz
+            FROM watchlist_items
+            UNION ALL
+            SELECT media_key,
+                   media_type,
+                   details_title_media_type,
+                   playback_parent_provider,
+                   playback_parent_provider_id,
+                   title,
+                   subtitle,
+                   poster_url,
+                   backdrop_url,
+                   details_release_year,
+                   details_rating,
+                   NULL::timestamptz,
+                   NULL::timestamptz,
+                   NULL::timestamptz,
+                   rated_at
+            FROM ratings
+        ) seeded_rows
+    ) deduped_rows
+    WHERE row_rank = 1
+      AND title IS NOT NULL
+      AND poster_url IS NOT NULL
+) cache_rows
+WHERE title_provider <> ''
   AND title_provider_id <> ''
 ON CONFLICT (media_key)
 DO UPDATE SET
