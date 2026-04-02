b34049e3efb955c1c47ceb2687450c1e094278aa
Aayush Rautela <rautela4488@gmail.com>
2026-04-02 16:03:29 +0200

    fix: drop dependent tables before watch_events in migration 0028

--- migrations/0028_drop_legacy_watch_tables.sql
+++ migrations/0028_drop_legacy_watch_tables.sql
@@ -4,5 +4,5 @@ DROP TABLE IF EXISTS watch_history_latest;
 DROP TABLE IF EXISTS watchlist_items;
 DROP TABLE IF EXISTS ratings;
 DROP TABLE IF EXISTS media_progress;
-DROP TABLE IF EXISTS watch_events;
 DROP TABLE IF EXISTS profile_tracked_series;
+DROP TABLE IF EXISTS watch_events;
